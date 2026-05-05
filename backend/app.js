1f72a90f4307ed4f6c6e8f859492e44f8b925fea
Behram035 <behramkh035@gmail.com>
2026-05-05 18:04:50 +0500

    get user profile by cookie and headers authorization token

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const ConnectDB = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 
@@ -10,6 +11,7 @@ ConnectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
