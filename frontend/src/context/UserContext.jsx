10a4361e26db89f794395c050bd1a82643800533
Behram035 <behramkh035@gmail.com>
2026-05-06 13:06:38 +0500

    add user context

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,11 +1,20 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const UserDataContext = createContext();
 
 const UserContext = ({ children }) => {
+  const [user, setUser] = useState({
+    email: "",
+    fullName: {
+      firstName: "",
+      lastName: "",
+    },
+  });
   return (
     <div>
-      <UserDataContext.Provider>{children}</UserDataContext.Provider>
+      <UserDataContext.Provider value={[user, setUser]}>
+        {children}
+      </UserDataContext.Provider>
     </div>
   );
 };
