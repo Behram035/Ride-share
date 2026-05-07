3b1c27f294cb82da7c9d0e38eed96cc3500aaee5
Behram035 <behramkh035@gmail.com>
2026-05-08 00:29:46 +0500

    Add Looking for driver and Waiting for driver components

--- frontend/src/components/LookingForDriver.jsx
+++ frontend/src/components/LookingForDriver.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const ConfirmRide = (props) => {
+const LookingForDriver = (props) => {
   return (
     <div>
       <h5
         className="p-1 text-center w-[93%] absolute top-0"
         onClick={() => {
-          props.setConfirmRidePanel(false);
+          props.setVehicleFound(false);
         }}
       >
         <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
       </h5>
-      <h3 className="text-2xl font-semibold mb-5">Confirm your Ride</h3>
+      <h3 className="text-2xl font-semibold mb-5">Looking for a Driver</h3>
 
       <div className="flex gap-2 justify-between flex-col items-center">
         <img
@@ -25,7 +25,7 @@ const ConfirmRide = (props) => {
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
               <p className="text-sm -mt-1 text-gray-600">
-                21C, Rawalpindi, islamabad
+                21C, Rawalpindi, Islamabad
               </p>
             </div>
           </div>
@@ -34,30 +34,21 @@ const ConfirmRide = (props) => {
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
               <p className="text-sm -mt-1 text-gray-600">
-                20B, Rawalpindi, islamabad
+                20B, Rawalpindi, Islamabad
               </p>
             </div>
           </div>
           <div className="flex items-center gap-5 p-3">
             <i className="ri-currency-line"></i>
             <div>
-              <h3 className="text-lg font-medium">PKR 200 </h3>
+              <h3 className="text-lg font-medium">PKR 300 </h3>
               <p className="text-sm -mt-1 text-gray-600">Cash</p>
             </div>
           </div>
         </div>
-        <button
-          onClick={() => {
-            props.setVehicleFound(true);
-            props.setConfirmRidePanel(false);
-          }}
-          className="w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg"
-        >
-          Confirm Ride
-        </button>
       </div>
     </div>
   );
 };
 
-export default ConfirmRide;
+export default LookingForDriver;
